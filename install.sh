52a7ee3d69b98181cfbdd04477381ec761cf8952
Shangyint <shangyin@berkeley.edu>
2022-11-30 13:28:40 -0800

    update install sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 pip3 install -e ".[dev]"
 
 cd build_jaxlib
-CC=/usr/bin/gcc-8 python3 build/build.py --enable_cuda --dev_install --bazel_options=--override_repository=org_tensorflow=$(pwd)/../third_party/tensorflow-alpa
+python3 build/build.py --enable_cuda --dev_install --bazel_options=--override_repository=org_tensorflow=$(pwd)/../third_party/tensorflow-alpa
 cd dist
 
 pip3 install -e .
